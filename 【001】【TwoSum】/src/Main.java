b7622bb2defd55d2dd3c3ff6cdd888614ec5f54f
xuyonghai123 <120961361@qq.com>
2017-03-30 22:55:25 +0800

    leetcode

---
+++ 【001】【TwoSum】/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Íõ¿¡³¬
+ * Date: 2015-06-18
+ * Time: 09:17
+ * Declaration: All Rights Reserved !!!
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[] result = new Solution().twoSum(new int[]{2, 7, 3, 6}, 9);
+
+        System.out.println("[" + result[0] + " ," + result[1] + "]");
+    }
+}
